7f389eccf0d8e485e3b7d2fdf7c0efd43b46c047
yusufhnf <flyme.yusuf@gmail.com>
2020-05-01 22:38:48 +0700

    Updates

---
+++ precache-manifest.36a487bcb7abaa5f80b7e23164a0f93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4067bd69d4163f993b04f5f6c3faab",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8344b2fb806666b3a156",
+    "url": "/resume/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1868546b76755d13629b",
+    "url": "/resume/static/js/2.cdefec29.chunk.js"
+  },
+  {
+    "revision": "8344b2fb806666b3a156",
+    "url": "/resume/static/js/main.7b8c9e6d.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
